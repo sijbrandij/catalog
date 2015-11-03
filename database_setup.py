2d9cb114e211320fa8835679def022fa65a8b155
karen <karen.sijbrandij@gmail.com>
2015-11-03 13:06:12 -0800

    When deleting a category, also delete its items.

--- database_setup.py
+++ database_setup.py
@@ -22,6 +22,7 @@ class Category(Base):
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+    items = relationship('Item', cascade='all, delete', backref='Category')
 
     @property
     def serialize(self):
